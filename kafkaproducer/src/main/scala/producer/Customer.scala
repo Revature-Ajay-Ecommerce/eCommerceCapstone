7810119c769fb70ee411232050bff20fffb5268b
Nicholas Young <no09young@gmail.com>
2022-05-04 10:00:11 -0500

    Revert "Added customer remembering functionality"
    This reverts commit 57a7d928bf8c2939dcd8c722ad3bfc108c005892.

--- kafkaproducer/src/main/scala/producer/Customer.scala
+++ kafkaproducer/src/main/scala/producer/Customer.scala
@@ -1,6 +1,7 @@
 package producer
 
 import scala.util.Random
+import scala.collection.mutable.ListBuffer
 
 class Customer {
   val names = List(
@@ -40,13 +41,42 @@ class Customer {
   )
 
   var nextCustomerID: Int = 1
+  var customerID = 0
 
   def generateCustomerInfo(): Tuple4[String, String, String, Int] = {
-    val getName = names(Random.nextInt(names.length))
-    val getLocation = locations(Random.nextInt(locations.length))
-    val customer = (getName, getLocation._1, getLocation._2, nextCustomerID)
+    var customerList = new ListBuffer[Tuple4[String, String, String, Int]]      //Initiate List of created customers
+    var customer = ("Name", "City", "Country", 0)
+    if(customerList.length < 5000){                                             //5000 set as max number of created Customers
+      val getName = names(Random.nextInt(names.length))
+      val getLocation = locations(Random.nextInt(locations.length))
+      for(cust <- customerList){    //Won't work for empty List                 //Iterate through customer List to check if existing
+        if(cust._1 == getName && cust._2 == getLocation._1 && cust._3 == getLocation._2){
+          customerID = cust._4
+        }
+        else{
+          customerID = nextCustomerID
+          customerList += Tuple4(getName, getLocation._1, getLocation._2, customerID)
+          nextCustomerID += 1
+        }
+      }
+      customer = Tuple4(getName, getLocation._1, getLocation._2, customerID)
+      if(customerList.length < 1){                                              //Only used for first customer to add to customer List
+        customerList += customer
+        nextCustomerID += 1
+      }
+    }
+    else{                                                                       //Pick from customer List if 5000 customers exist
+      customer = customerList(Random.nextInt(customerList.length))
+    }
+    //val customer = (getName, getLocation._1, getLocation._2, customerID)
     // println(customer)
-    nextCustomerID += 1
+    //nextCustomerID += 1
     return customer
+  }
+}
+
+object Main extends Customer {
+  for(i <- 0 to 10){
+    println(generateCustomerInfo())
   }
 }
