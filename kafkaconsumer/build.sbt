36fb91cff7aeda5f905b9c1181fecc32ac9d8105
Matt Scalzo <matthewfscalzo@gmail.com>
2022-05-11 12:39:10 -0400

    Fix Build file and trying stuff out

--- kafkaconsumer/build.sbt
+++ kafkaconsumer/build.sbt
@@ -1,6 +1,6 @@
 import Dependencies._
 
-ThisBuild / scalaVersion     := "2.13.6"
+ThisBuild / scalaVersion     := "2.11.8"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "com.ecommerce"
 ThisBuild / organizationName := "ecommerce"
@@ -10,16 +10,16 @@ lazy val root = (project in file("."))
   .settings(
     name := "kafkaconsumer",
     libraryDependencies += scalaTest % Test
-    ,libraryDependencies += "org.apache.spark" %% "spark-core" % "3.2.0"
-    ,libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.2.0"
-    ,libraryDependencies += "org.apache.spark" %% "spark-hive" % "3.2.0"
-    // ,libraryDependencies += "org.apache.hadoop" % "hadoop-client" % "2.7.0"
-    //Kafka
-    ,libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.2.0"
-    ,libraryDependencies += "org.apache.kafka" %% "kafka" % "3.1.0"
+    ,libraryDependencies += "org.apache.spark" %% "spark-core" % "1.2.0"
+    ,libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.1.0"
+    //,libraryDependencies += "org.apache.spark" %% "spark-streaming" % sparkVersion
+    //,libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.3"
+    //,libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.3"
+    // For kafka.
+    ,libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "2.3.4"
+    ,libraryDependencies += "org.apache.kafka" %% "kafka" % "2.1.0"
     // For Json.
-     // ,libraryDependencies += "net.liftweb" %% "lift-json" % "3.5.0"
-    
+    ,libraryDependencies += "net.liftweb" %% "lift-json" % "3.5.0"
   )
 
 // See https://www.scala-sbt.org/1.x/docs/Using-Sonatype.html for instructions on how to publish to Sonatype.
--- kafkaconsumer/src/main/scala/consumer/kafkaconsumer.scala
+++ kafkaconsumer/src/main/scala/consumer/kafkaconsumer.scala
@@ -12,40 +12,82 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{Column, Dataset, Row, SparkSession}
 
 object consumer {
-  def main(args: Array[String]): Unit = {
-    val spark: SparkSession = SparkSession
-      .builder()
-      .master("local[4]")
-      .appName("consumer")
-      .getOrCreate()
+  // def main(args: Array[String]): Unit = {
+  //   val spark: SparkSession = SparkSession
+  //     .builder()
+  //     .master("local[*]")
+  //     .appName("consumer")
+  //     .getOrCreate()
 
-      import spark.implicits._
-   // spark.sparkContext.setLogLevel("ERROR")
-   // val sc = spark.sparkContext
+  //     import spark.implicits._
+  //  // spark.sparkContext.setLogLevel("ERROR")
+  //  // val sc = spark.sparkContext
 
-   val col1 = new StructType()
-        .add("claim_id", StringType, true)
-        .add("customer_id", StringType, true)
-        .add("customer_name", StringType, true)
-        .add("Customer_age", StringType, true)
-        .add("agent_id", StringType, true)
-        .add("agent_name", StringType, true)
-        .add("claim_category", StringType, true)
-        .add("amount", StringType, true)
-        .add("reason", StringType, true)
-        .add("agent_rating", StringType, true)
-        .add("datetime", StringType, true)
-        .add("country", StringType, true)
-        .add("state", StringType, true)
-        .add("approval", StringType, true)
-        .add("reimbursement_id", StringType, true)
-        .add("failure_reason", StringType, true)
+  //  val schema = new StructType()
+  //       .add("claim_id", StringType, true)
+  //       .add("customer_id", StringType, true)
+  //       .add("customer_name", StringType, true)
+  //       .add("customer_age", StringType, true)
+  //       .add("agent_id", StringType, true)
+  //       .add("agent_name", StringType, true)
+  //       .add("claim_category", StringType, true)
+  //       .add("amount", StringType, true)
+  //       .add("reason", StringType, true)
+  //       .add("agent_rating", StringType, true)
+  //       .add("datetime", StringType, true)
+  //       .add("country", StringType, true)
+  //       .add("state", StringType, true)
+  //       .add("approval", StringType, true)
+  //       .add("reimbursement_id", StringType, true)
+  //       .add("failure_reason", StringType, true)
 
-    val df = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667").option("subscribe", "ecommerce").load.select(col("value").cast("string"))
+  //   val df1 = spark.readStream.format("kafka").option("kafka.bootstrap.servers", "sandbox-hdp.hortonworks.com:6667").option("subscribe", "insurance").load().select(col("value").cast("string"))
 
-        val cdataframe = df.select(from_json(col("value"), col1))
-        cdataframe.printSchema()
+  //   val df2 = df1.selectExpr("CAST (value AS STRING)").toDF("value")
 
-        cdataframe.writeStream.format("console").outputMode("append").option("truncate", "false").start().awaitTermination()
-  }
+  //   val df3 = df2.select(from_json(col("value"), schema))
+  //   df3.printSchema()
+
+  //   df3.writeStream.format("console").outputMode("append").option("truncate", "false").start().awaitTermination()
+
+  //   //df3.write.csv("~/insuranceOut.csv")
+
+  // }
+
+  def main(args: Array[String]):Unit = {
+        val props: Properties = new Properties()
+        props.put("group.id", "test-consumer-group") //need group id from
+        props.put("bootstrap.servers", "sandbox-hdp.hortonworks.com:6667")
+        props.put(
+        "key.deserializer",
+        "org.apache.kafka.common.serialization.StringDeserializer"
+        )
+        props.put(
+        "value.deserializer",
+        "org.apache.kafka.common.serialization.StringDeserializer"
+        )
+        props.put("enable.auto.commit", "true")
+        props.put("auto.commit.interval.ms", "2000")
+        val consumer = new KafkaConsumer(props)
+        val topics = List("insurance")
+        try {
+        consumer.subscribe(topics.asJava) //subscribe to topic as a type Java
+        while (true) {
+            val records = consumer.poll(10) //read one record every 10 seconds
+            for (record <- records.asScala) { // convert the read records into Scala code
+              println( //for each record print Topic, Key, Value, Offset, Partiton
+                // "Topic: " + record.topic() +
+                ",Key: " + record.key() +
+                ",Value: " + record.value() +
+                // ", Offset: " + record.offset() +
+                ", Partition: " + record.partition()
+              )
+            }
+        }
+        } catch {
+        case e: Exception => e.printStackTrace()
+        } finally {
+        consumer.close()
+        }
+    }
 }
